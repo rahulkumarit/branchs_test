09eb7cfd39b8e625fc35f0cd114aec98dab9e381
Rahul Kumar <rahulkumar@devrepublic.nl>
2018-05-17 17:46:23 +0530

    Push Notification

---
+++ app/src/main/java/com/dailogexample/notification/MyFirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package com.dailogexample.notification;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebase";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.e(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String refreshedToken) {
+
+    }
+
+
+}
